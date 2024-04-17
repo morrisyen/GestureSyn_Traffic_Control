f361388c5bcd6aa514c69f2386006583eb9ad52f
morrisyen <146398431+morrisyen@users.noreply.github.com>
2024-04-16 18:44:34 -0700

    Add files via upload

---
+++ RPI/setup.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt install build-essential
+sudo apt install libatlas-base-dev
+sudo apt install python3-pip
+pip3 install tflite-runtime
+
+sudo apt install -y python3-opencv
+# pip3 install opencv-python
+
+pip3 install pillow
+pip3 install numpy
+
