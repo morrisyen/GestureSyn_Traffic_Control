857c58f645571a93e19035b332571390b1be7f66
morrisyen <146398431+morrisyen@users.noreply.github.com>
2025-01-21 19:27:36 -0800

    Add files via upload

---
+++ STM32/Core/Inc/TrafficControl.h
@@ -0,0 +1,14 @@
+#ifdef cplusplus
+extern "C" {
+#endif
+
+#include "main.h"
+#include "CalculateAngle.h"
+
+void ConditionSelect(MPU6050_t* mpu6050);
+
+extern uint8_t receiveData[2];
+
+#ifdef cplusplus
+}
+#endif
---
+++ STM32/Core/Inc/mpu6050.h
@@ -0,0 +1,81 @@
+/*
+ * mpu6050.h
+ *
+ *  Created on: Nov 13, 2019
+ *      Author: Bulanov Konstantin
+ */
+
+#ifndef INC_GY521_H_
+#define INC_GY521_H_
+
+#endif /* INC_GY521_H_ */
+
+#include <stdint.h>
+#include "i2c.h"
+
+// MPU6050 structure
+typedef struct
+{
+
+    int16_t Accel_X_RAW;
+    int16_t Accel_Y_RAW;
+    int16_t Accel_Z_RAW;
+    double Ax;
+    double Ay;
+    double Az;
+
+    int16_t Gyro_X_RAW;
+    int16_t Gyro_Y_RAW;
+    int16_t Gyro_Z_RAW;
+    double Gx;
+    double Gy;
+    double Gz;
+
+    float Temperature;
+
+    double KalmanAngleX;
+    double KalmanAngleY;
+
+    double AngleX;
+    double AngleY;
+    double AngleZ;
+
+    float acc_roll;
+    float acc_pitch;
+
+    //Calibration
+	float gyro_cali_x;
+	float gyro_cali_y;
+	float gyro_cali_z;
+} MPU6050_t;
+
+extern MPU6050_t MPU6050;
+
+// Kalman structure
+typedef struct
+{
+    double Q_angle;
+    double Q_bias;
+    double R_measure;
+    double angle;
+    double bias;
+    double P[2][2];
+} Kalman_t;
+
+
+uint8_t MPU6050_Init(I2C_HandleTypeDef *I2Cx);
+
+void MPU6050_Read_Accel(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct);
+
+void MPU6050_Read_Gyro(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct);
+
+void MPU6050_Read_Temp(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct);
+
+void MPU6050_Read_All(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct);
+
+void MPU6050_Calibration(MPU6050_t* mpu6050);
+
+void MPU6050_Correction(MPU6050_t* mpu6050);
+
+//double Kalman_getAngle(Kalman_t *Kalman, double newAngle, double newRate, double dt);
+
